8b13baed2cb7bbcbd60edd211e078a1076f1998d
Spitzmuller.Julius <Spitzmuller.Julius@SK-8C89A5CC3877.2631.skool.net>
2017-10-10 15:36:51 +0200

    Commit

--- JavaSpiel/src/Testvertex.java
+++ JavaSpiel/src/Testvertex.java
@@ -4,8 +4,18 @@ public class Testvertex {
 public static void main(String[] args) {
 		
 		vertex v1 = new vertex(2,2);
+		vertex v2 = v1.skalarMult(3);
+		vertex v3 = v1.add(v2);
+		
 		System.out.println(v1);
-		System.out.println(Lenght(v1));		
+		System.out.println(v2);
+		System.out.println(v3);
+		
+		v1.addMod(v3);
+	
+		System.out.println(v1);
+		
+		System.out.println(v1.lenght());		
 	}
 	
 }
--- JavaSpiel/src/vertex.java
+++ JavaSpiel/src/vertex.java
@@ -3,23 +3,71 @@ public class vertex {
 	public double x;
 	public double y;
 	
+	
 	public vertex(double x, double y){
 		
 		this.x = x;
 		this.y = y;
-		
 	}
 
+	
 	public String toString(){
 		
 		return "("+x+","+y+")";
-		
 	}
 	
-	public double lenght(vertex v){
+	
+	public double lenght(){
 		
-	    return Math.sqrt(v.x*v.x+v.y*v.y);
-		
+	    return Math.sqrt(x*x+y*y);
 	}
 	
+	
+	public vertex skalarMult(double s) {
+		
+		return new vertex(x*s,y*s);
+	}
+	
+	
+    public void skalarMultMod(double s) {
+		
+		x=x*s;
+		y=y*s;
+	}
+	
+    
+    public vertex add(vertex v){
+    	
+    	return new vertex (v.x+x, v.y+y);
+    }
+
+    
+    public void addMod(vertex v) {
+		
+	    x = v.x + x;
+	    y = v.y + y;
+    }
+    
+    
+    public void setX(double x){
+    	
+    	this.x = x;
+    }
+    
+    
+    public void setY(double y){
+    	
+    	this.y = y;
+    }
+    
+    
+    public double getX() {
+    	return x;
+    }
+
+
+    public double getY() {
+    	return y;
+    }
 }
+
