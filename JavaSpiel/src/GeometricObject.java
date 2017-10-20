619a9a21afe6674aebc13fa113225deb82378a1c
Spitzmuller.Julius <Spitzmuller.Julius@SK-8C89A5CC3877.2631.skool.net>
2017-10-20 08:23:38 +0200

    Commit

--- JavaSpiel/src/GeometricObject.java
+++ JavaSpiel/src/GeometricObject.java
@@ -1,8 +1,53 @@
 
 public class GeometricObject {
 	
+
 	
-	public double getWidth() {return Width;}
-	public double getHeight() {return Height;}
-	public Vertex getPos() {return Pos;}
+	public vertex pos;
+	public double width;
+	public double height;
+	
+	public double getwidth() {return width;}
+	public double getheight() {return height;}
+	public vertex getpos() {return pos;}
+	
+	public GeometricObject(vertex pos, double width, double height){
+		
+		this.pos = pos;
+		this.width = width;
+	    this.height = height;
+	    
+	    if(width <0){
+	    	
+	    	width = -width;
+	    	pos.x = pos.x-width;
+	    }
+	    
+
+	    if(height <0){
+	    	
+	    	height = -height;
+	    	pos.y = pos.y-height;
+	    }
+	}
+	
+public GeometricObject(double width, double height){
+	
+	this(new vertex(0, 0), width, height);
+}
+
+public GeometricObject(double width){
+	
+	this(width, width);
+}
+
+public GeometricObject(){
+	
+	this(10);
+}
+
+public GeometricObject(vertex pos){
+	
+	this(pos, 0, 0);
+}
 }
